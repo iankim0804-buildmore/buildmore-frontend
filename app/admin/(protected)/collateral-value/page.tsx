2a77ca5c138450d9270d59b6d1961f735e286452
iankim0804-buildmore <iankim0804-buildmore@users.noreply.github.com>
2026-05-16 04:11:28 +0900

    Restore admin data coverage layout

--- app/admin/(protected)/collateral-value/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CollateralValueSection } from '../components/collateral-value-section'
-
-export default function AdminCollateralValuePage() {
-  return (
-    <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <CollateralValueSection />
-    </main>
-  )
-}
